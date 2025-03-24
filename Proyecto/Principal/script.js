3ac86b44f4c51d135095fa0a6520cb59e6e16f0c
demonhd64 <daniel.encinas.martin@gmail.com>
2025-03-24 21:03:55 +0100

    modified:   Proyecto/Principal/index.html 	modified:   Proyecto/Principal/script.js 	modified:   Proyecto/Principal/style.css

---
+++ Proyecto/Principal/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggle = document.getElementById('toggle');
+    const desplegable = document.querySelector('.desplegable');
+    const button = document.querySelector('button'); // Selecciona el botón
+
+    // Cambia el estado del checkbox y muestra/oculta el desplegable al hacer clic en el botón
+    button.addEventListener('click', function() {
+        toggle.checked = !toggle.checked; // Cambia el estado del checkbox
+        if (toggle.checked) {
+            desplegable.style.display = 'block';
+        } else {
+            desplegable.style.display = 'none';
+        }
+    });
+
+    // Cerrar el desplegable si se hace clic fuera de él
+    document.addEventListener('click', function(event) {
+        const isClickInside = toggle.contains(event.target) || desplegable.contains(event.target) || button.contains(event.target);
+        if (!isClickInside) {
+            toggle.checked = false; // Desmarcar el checkbox
+            desplegable.style.display = 'none'; // Ocultar el desplegable
+        }
+    });
+});
