8b0345e1ce2e3cda36c5647f12e2518c42bc473a
demonhd64 <daniel.encinas.martin@gmail.com>
2025-03-26 19:42:24 +0100

    modified:   Proyecto/Secundarias/JS/desayuno.js 	modified:   Proyecto/Secundarias/css/desayuno.css

--- Proyecto/Secundarias/JS/desayuno.js
+++ Proyecto/Secundarias/JS/desayuno.js
@@ -20,3 +20,28 @@ function MirarSiActivo(el) {
         }
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toggle = document.getElementById('activo');
+    const desplegable = document.querySelector('.desplegable');
+    const button = document.querySelector('button'); // Selecciona el botón
+
+    // Cambia el estado del checkbox y muestra/oculta el desplegable al hacer clic en el botón
+    button.addEventListener('click', function() {
+        toggle.checked = !toggle.checked; // Cambia el estado del checkbox
+        if (toggle.checked) {
+            desplegable.style.display = 'flex';
+        } else {
+            desplegable.style.display = 'none';
+        }
+    });
+
+    // Cerrar el desplegable si se hace clic fuera de él
+    document.addEventListener('click'/* Comprueba si se hace la opcion click y ejecuta la funcion siguiente */, function(event) {
+        const isClickInside = toggle.contains(event.target) || desplegable.contains(event.target) || button.contains(event.target); // Opciones permitidas de hacer click dentro. (Los nombres son los de las constantes)
+        if (!isClickInside) { // Si se detecta el click en cualquier otro que no sea el de los permitidos se desmarca el checkbox y el display en none.
+            toggle.checked = false; // Desmarcar el checkbox
+            desplegable.style.display = 'none'; // Ocultar el desplegable
+        }
+    });
+});
