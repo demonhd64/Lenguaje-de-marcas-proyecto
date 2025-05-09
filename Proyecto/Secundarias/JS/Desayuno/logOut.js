3b20f56924f41c440e33346826aef89441497f65
demonhd64 <daniel.encinas.martin@gmail.com>
2025-05-09 19:25:21 +0200

    modified:   Proyecto/Secundarias/JS/Desayuno/logOut.js 	modified:   Proyecto/Secundarias/css/desayuno.css

--- Proyecto/Secundarias/JS/Desayuno/logOut.js
+++ Proyecto/Secundarias/JS/Desayuno/logOut.js
@@ -7,14 +7,10 @@ const LogOut = document.getElementById("LogOut")
 const forgotpassw = document.querySelector("#Forgotpassw")
 
 LogOut.addEventListener('click', async ()=>{
-    try {
         let email = auth.currentUser.displayName || auth.currentUser.email.split("@")[0] || auth.currentUser.email//Obtener el email cuando se pulse el boton, de modo que ya este guardado
         let fotosUsers = auth.currentUser?.photoURL
         mensajes(`Se ha cerrado la sesión del usuario ${ email }`, "success", fotosUsers);
         errores.ContadorErrores = 0 //Reiniciar el contador de errores al cerrar sesión
         forgotpassw.style.display = "none" //Ocultar el botón de restablecimiento de contraseña al cerrar sesión
         await deleteUser(auth.currentUser) //Eliminar el usuario de la autenticación Ademas esto cierra sesion   
-    } catch (error) {
-        
-    }
 })
